c39d7edf8d18f3fe8fcdb6987019d8e5496759dc
vfil <wladyslawfil@gmail.com>
2023-05-19 11:39:17 +0300

    Move parsers examples from Lik

---
+++ ParsingPerformance/ParsingPerformance/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  stdin
+//
+//  Created by  Vladyslav Fil on 05.02.2023.
+//
+
+import Foundation
+import Benchmark
+
+
+
+Benchmark.main([
+//    copyingBenchmarkSuite
+//    intParsingBenchmarkSuite
+    raceSuite
+])
